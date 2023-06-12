94ffff4c77338ce6459ee0472c6ee167a3191955
joe10111 <centeno.joe@gmail.com>
2023-06-12 13:04:14 -0700

    Stores and reads from data Database throus user input

--- MessageLogger/Models/Message.cs
+++ MessageLogger/Models/Message.cs
@@ -9,13 +9,8 @@ namespace MessageLogger.Models
     public class Message
     {
         public int Id { get; set; }
-        public string Content { get; private set; }
-        public DateTime CreatedAt { get; private set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
 
-        public Message(string content)
-        {
-            Content = content;
-            CreatedAt = DateTime.Now;
-        }
     }
 }
