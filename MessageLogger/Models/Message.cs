e1e6daed6d2e82215ef4ecd6cd60c840d8e37dd8
joe <centeno.joe@gmail.com>
2023-06-13 19:09:45 -0700

    Refactored main

--- MessageLogger/Models/Message.cs
+++ MessageLogger/Models/Message.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using MessageLogger.Models;
 namespace MessageLogger.Models
 {
     public class Message
@@ -11,5 +11,7 @@ namespace MessageLogger.Models
         public int Id { get; set; }
         public string Content { get; set; }
         public DateTime CreatedAt { get; set; }
+
+        //public User MessageUser { get; } 
     }
 }
